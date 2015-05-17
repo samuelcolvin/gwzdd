2fee3edc3974042c15e6eabe67c97fbc38f9f52e
Samuel Colvin <s@muelcolvin.com>
2015-05-17 21:23:57 +0100

    more fiddling with network test

--- mq/network.py
+++ mq/network.py
@@ -1,11 +1,12 @@
 import time
 import random
+from multiprocessing import Process
 import zmq.green as zmq
 import gevent
 from threading import Thread
 
 # each node is identified by it's port
-NODES = range(20)
+NODES = range(50)
 
 
 class TimeoutException(Exception):
@@ -16,76 +17,96 @@ def get_port(node_id):
     return 16000 + node_id
 
 
-server_address_base = 'tcp://*:%d'
-client_address_base = 'tcp://localhost:%d'
+# server_address_base = 'tcp://*:%d'
+# client_address_base = 'tcp://localhost:%d'
 
 server_address_base = 'ipc:///tmp/feeds_%d'
 client_address_base = server_address_base
 
 
-def server(ctx, node_id, shared):
-    rep_socket = ctx.socket(zmq.REP)
-    rep_socket.bind(server_address_base % get_port(node_id))
-    rep_socket.set_hwm(100000)
-    timeout = gevent.Timeout(2, TimeoutException)
-    start = time.time()
-    new_messages = 0
-    while True:
-        if not rep_socket.poll(1000):
-            continue
-        timeout.start()
-        try:
-            message = rep_socket.recv_json()
-            rep_socket.send('OK %r' % message)
-        except TimeoutException:
-            pass
-        finally:
-            timeout.cancel()
-        new_messages += 1
-        if (time.time() - start) > 1:
-            if node_id % 10 == 0:
-                print 'node %d: server msg count %d, job_no: %d' % (node_id, new_messages, shared['job_no'])
-            start = time.time()
-            new_messages = 0
-        gevent.sleep(0)
+class Node(object):
+    def __init__(self, node_id):
+        self.node_id = node_id
+        self.shared_job_no = 0
+        self.go = True
 
+    def run_gevent(self):
+        ctx = zmq.Context()
+        print 'starting %s' % self.node_id
+        g = gevent.spawn(self.server, ctx, self.node_id)
+        self.client(ctx, self.node_id)
+        print '%d node: client finished' % self.node_id
+        gevent.sleep(5)
+        self.go = False
+        g.kill()
+        # print '%d node: exiting' % self.node_id
 
-def client(ctx, this_node_id, shared):
-    timeout = gevent.Timeout(5, TimeoutException)
-    for job_no in range(100):
-        for node_id in NODES:
-            if node_id == this_node_id:
+    def run_threading(self):
+        ctx = zmq.Context()
+        print 'starting %s' % self.node_id
+        Thread(target=self.server, args=(ctx, self.node_id)).start()
+        self.client(ctx, self.node_id)
+        gevent.sleep(5)
+        self.go = False
+        print '%d node: client finished' % self.node_id
+
+    def server(self, ctx, node_id):
+        rep_socket = ctx.socket(zmq.REP)
+        rep_socket.bind(server_address_base % get_port(node_id))
+        rep_socket.set_hwm(100000)
+        timeout = gevent.Timeout(2, TimeoutException)
+        start = time.time()
+        new_messages = 0
+        while True:
+            if not self.go:
+                break
+            if not rep_socket.poll(1000):
                 continue
-            socket = ctx.socket(zmq.REQ)
-            socket.set_hwm(100000)
             timeout.start()
             try:
-                socket.connect(client_address_base % get_port(node_id))
-                socket.send_json({'source': this_node_id, 'job_no': job_no})
-                socket.recv()
+                message = rep_socket.recv_json()
+                rep_socket.send('OK %r' % message)
             except TimeoutException:
-                # this normally only happens at the end of a run
-                print 'timeout on node %d client, job %d, killing client' % (this_node_id, job_no)
-                return
+                pass
+            else:
+                new_messages += 1
             finally:
                 timeout.cancel()
-                socket.close()
-        shared['job_no'] = job_no
-        gevent.sleep(0.05 + random.random()*0.05)
+            if (time.time() - start) > 1:
+                if node_id % 10 == 0:
+                    print 'node %d: server msg count %d, job_no: %d' % (node_id, new_messages, self.shared_job_no)
+                start = time.time()
+                new_messages = 0
+
+    def client(self, ctx, this_node_id):
+        timeout = gevent.Timeout(10, TimeoutException)
+        for job_no in range(100):
+            for node_id in NODES:
+                if node_id == this_node_id:
+                    continue
+                socket = ctx.socket(zmq.REQ)
+                socket.set_hwm(100000)
+                timeout.start()
+                try:
+                    socket.connect(client_address_base % get_port(node_id))
+                    socket.send_json({'source': this_node_id, 'job_no': job_no})
+                    socket.recv()
+                except TimeoutException:
+                    # this normally only happens at the end of a run
+                    print 'timeout on node %d client, job %d' % (this_node_id, job_no)
+                    return
+                finally:
+                    timeout.cancel()
+                    socket.close()
+            self.shared_job_no = job_no
+            gevent.sleep(0.1 + random.random()*0.1)
 
 
 def run_node(node_id):
-    ctx = zmq.Context()
-    print 'starting %s' % node_id
-    shared = {'job_no': 0}
-    g = gevent.spawn(server, ctx, node_id, shared)
-    client(ctx, node_id, shared)
-    print '%d node: client finished' % node_id
-    gevent.sleep(5)
-    g.kill()
-    print '%d node: exiting' % node_id
+    # Node(node_id).run_threading()
+    Node(node_id).run_gevent()
 
 if __name__ == '__main__':
     for node_id in NODES:
-        Thread(target=run_node, args=(node_id,)).start()
+        Process(target=run_node, args=(node_id,)).start()
     print 'started %d nodes' % len(NODES)
